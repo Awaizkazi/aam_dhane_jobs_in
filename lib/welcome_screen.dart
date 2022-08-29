ae87ade665ca231721337d722f1c8e0f25e9f870
Awaizkazi <88947135+Awaizkazi@users.noreply.github.com>
2022-08-29 21:11:17 +0530

    c

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -12,7 +13,8 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
+        body: SingleChildScrollView(
+      child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 100),
         child: Column(
           children: [
@@ -28,9 +30,140 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               'Sign IN / Sign UP',
               style: TextStyle(fontSize: 20, color: Colors.grey.shade700),
             ),
+            TextField(),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'Get OTP',
+                style: TextStyle(fontSize: 20),
+              ),
+              style: ElevatedButton.styleFrom(
+                  minimumSize: Size(350, 50),
+                  primary: Colors.green[900],
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10))),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              'You will receive 4 digit code on your phone. Please verify your phone by putting the code here ',
+              style: TextStyle(fontSize: 16),
+            ),
+
+            // Text Form Field of 4 Boxes
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: SizedBox(
+                      height: 70,
+                      width: 50,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.0)),
+                          // hintText: 'Please enter a search term'
+                        ),
+                        style: Theme.of(context).textTheme.headline6,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(1),
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: SizedBox(
+                      height: 70,
+                      width: 50,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.0)),
+                          // hintText: 'Please enter a search term'
+                        ),
+                        style: Theme.of(context).textTheme.headline6,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(1),
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: SizedBox(
+                      height: 70,
+                      width: 50,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.0)),
+                          // hintText: 'Please enter a search term'
+                        ),
+                        style: Theme.of(context).textTheme.headline6,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(1),
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: SizedBox(
+                      height: 70,
+                      width: 50,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.0)),
+                          // hintText: 'Please enter a search term'
+                        ),
+                        style: Theme.of(context).textTheme.headline6,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(1),
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'Next',
+                  style: TextStyle(fontSize: 20, color: Colors.black54),
+                ),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size(350, 50),
+                    primary: Color.fromARGB(255, 218, 231, 219),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))),
+              ),
+            ),
           ],
         ),
       ),
-    );
+    ));
   }
 }
